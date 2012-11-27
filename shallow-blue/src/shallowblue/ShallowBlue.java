2b30d31cd6f6129b4e6d7daf51604290e2e3c274
Justin Svegliato <justin.svegliato1@marist.edu>
2012-11-26 20:35:01 -0500

    Amended test print

--- shallow-blue/src/shallowblue/ShallowBlue.java
+++ shallow-blue/src/shallowblue/ShallowBlue.java
@@ -14,6 +14,6 @@ public class ShallowBlue {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Test commit");
+        System.out.println("Another test commit");
     }
 }
