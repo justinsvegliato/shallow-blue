20b8d8b1785622e73f89174cec2da00df9fd61c4
Justin Svegliato <justin.svegliato1@marist.edu>
2012-11-27 21:56:28 -0500

    Redesigned the implementation of the board

--- shallow-blue/src/shallowblue/ChessBoard.java
+++ shallow-blue/src/shallowblue/ChessBoard.java
@@ -6,63 +6,74 @@ public class ChessBoard {
     ChessBoard board = new ChessBoard();
     System.out.println(board);
   }
-  
-  private byte[][] board = new byte[8][8];
-
-  private static enum Pieces {
-    KING(1, 0), QUEEN(2, 9), ROOK(3, 5), KNIGHT(4, 3), BISHOP(5, 3), PAWN(6, 1);
-    
-    private final int value;
-    private final int worth;
-
-    private Pieces(int value, int weight) {
-      this.value = value;
-      this.worth = weight;
-    }
-
-    public int getValue() {
-      return value;
-    }
-
-    public int getWorth() {
-      return worth;
-    }
-  }
 
   public ChessBoard() {
-    for (int file = 0; file <= 7; file++) {
-      board[1][file] = (byte) Pieces.PAWN.getValue();
-      board[6][file] = (byte) -Pieces.PAWN.getValue();
-    }
-    
-    for (int offset = 0, piece = 3; offset < 3; offset++, piece++) {
-      board[0][offset] = (byte) piece;
-      board[0][7 - offset] = (byte) piece;
-      board[7][offset] = (byte) -piece;
-      board[7][7 - offset] = (byte) -piece;
+    for (int i = 0; i < 8; i++) {
+      board[8 + i] = pawn;
+      board[48 + i] = -pawn;
     }
 
-    board[0][3] = (byte) Pieces.KING.getValue();
-    board[7][3] = (byte) Pieces.KING.getValue();
-    board[0][4] = (byte) Pieces.QUEEN.getValue();
-    board[7][4] = (byte) Pieces.QUEEN.getValue();
+    board[0] = rook;
+    board[1] = knight;
+    board[2] = bishop;
+    board[7] = rook;
+    board[6] = knight;
+    board[5] = bishop;
+    board[3] = king;
+    board[4] = queen;
+
+    board[56] = -rook;
+    board[57] = -knight;
+    board[58] = -bishop;
+    board[63] = -rook;
+    board[62] = -knight;
+    board[61] = -bishop;
+    board[59] = -king;
+    board[60] = -queen;
+  }
+  private static byte[] board = new byte[64];
+  private static final byte king = 1;
+  private static final byte queen = 2;
+  private static final byte rook = 3;
+  private static final byte knight = 4;
+  private static final byte bishop = 5;
+  private static final byte pawn = 6;
+
+  public byte getWorth(byte piece) {
+    switch (piece) {
+      case 1:
+      case -1:
+        return 127;
+      case 2:
+      case -2:
+        return 9;
+      case 3:
+      case -3:
+        return 5;
+      case 4:
+      case 5:
+      case -4:
+      case -5:
+        return 3;
+      case 6:
+      case -6:
+        return 1;
+    }
+    return -1;
   }
 
-  @Override
   public String toString() {
     StringBuilder builder = new StringBuilder();
     builder.append("-------------------------\n");
-    for (int rank = 0; rank < 8; rank++) {
+    for (int i = 0; i < board.length; i++) {
       builder.append("|");
-      for (int file = 0; file < 8; file++) {
-        byte current = board[rank][file];
-        if (current >= 0) {
-          builder.append(" ");
-        }
-        builder.append(current);
-        builder.append("|");
+      if (board[i] >= 0) {
+        builder.append(" ");
       }
-      builder.append("\n-------------------------\n");
+      builder.append(board[i]);
+      if ((i + 1) % 8 == 0) {
+        builder.append("|\n-------------------------\n");
+      }
     }
     return builder.toString();
   }
