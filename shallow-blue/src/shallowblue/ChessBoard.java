749313a28bf7a0bcf4f3b53ca907c2d30611ea97
Justin Svegliato <justin.svegliato1@marist.edu>
2012-11-27 23:48:10 -0500

    Added border elements to the representation of the chess board

--- shallow-blue/src/shallowblue/ChessBoard.java
+++ shallow-blue/src/shallowblue/ChessBoard.java
@@ -8,36 +8,49 @@ public class ChessBoard {
   }
 
   public ChessBoard() {
-    for (int i = 0; i < 8; i++) {
-      board[8 + i] = pawn;
-      board[48 + i] = -pawn;
+    for (int i = 0; i < 10; i++) {
+      board[0 + i] = border;
+      board[10 + i] = border;
+      board[100 + i] = border;
+      board[110 + i] = border;
     }
+    
+    
+    for (int i = 0; i < 8; i++) {
+      board[20 + i * 10] = border;
+      board[29 + i * 10] = border;
+      board[31 + i] = pawn;
+      board[81 + i] = -pawn;
+    }    
 
-    board[0] = rook;
-    board[1] = knight;
-    board[2] = bishop;
-    board[7] = rook;
-    board[6] = knight;
-    board[5] = bishop;
-    board[3] = king;
-    board[4] = queen;
+    board[21] = rook;
+    board[22] = knight;
+    board[23] = bishop;
+    board[28] = rook;
+    board[27] = knight;
+    board[26] = bishop;
+    board[24] = king;
+    board[25] = queen;
 
-    board[56] = -rook;
-    board[57] = -knight;
-    board[58] = -bishop;
-    board[63] = -rook;
-    board[62] = -knight;
-    board[61] = -bishop;
-    board[59] = -king;
-    board[60] = -queen;
+    board[91] = -rook;
+    board[92] = -knight;
+    board[93] = -bishop;
+    board[98] = -rook;
+    board[97] = -knight;
+    board[96] = -bishop;
+    board[94] = -king;
+    board[95] = -queen;
   }
-  private static byte[] board = new byte[64];
+  
+  private static byte[] board = new byte[120];
+  
   private static final byte king = 1;
   private static final byte queen = 2;
   private static final byte rook = 3;
   private static final byte knight = 4;
   private static final byte bishop = 5;
   private static final byte pawn = 6;
+  private static final byte border = 7;
 
   public byte getWorth(byte piece) {
     switch (piece) {
@@ -66,17 +79,18 @@ public class ChessBoard {
     return board[piece];
   }
 
+  @Override
   public String toString() {
     StringBuilder builder = new StringBuilder();
-    builder.append("-------------------------\n");
+    builder.append("-------------------------------\n");
     for (int i = 0; i < board.length; i++) {
       builder.append("|");
       if (board[i] >= 0) {
         builder.append(" ");
       }
       builder.append(board[i]);
-      if ((i + 1) % 8 == 0) {
-        builder.append("|\n-------------------------\n");
+      if ((i + 1) % 10 == 0) {
+        builder.append("|\n-------------------------------\n");
       }
     }
     return builder.toString();
