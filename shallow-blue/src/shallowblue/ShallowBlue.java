e1d88d6d9ebdabd451d769b9c6ffd8a7c4bd0542
Justin Svegliato <justin.svegliato1@marist.edu>
2012-12-07 03:03:35 -0500

    Added server interaction

--- shallow-blue/src/shallowblue/ShallowBlue.java
+++ shallow-blue/src/shallowblue/ShallowBlue.java
@@ -3,50 +3,78 @@ package shallowblue;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 import shallowblue.ChessBoard.Player;
 
 public class ShallowBlue {
+
   private static ChessBoard board = new ChessBoard();
 
   public static void main(String[] args) throws IOException, JSONException, InterruptedException {
-    final String id = "21";
-    final int player = 1;
-    final String password = "32c68cae";
-    final int color = Player.BLACK.value;
-    
+    // Must update these for each game
+    final String gameId = "42";
+    int color = Player.WHITE.value;
+
+    final String password = "a923cc0";
+    final int team = 15;
+    final String baseUrl = "http://www.bencarle.com/chess/";
+    final String credentials = gameId + "/" + team + "/" + password + "/";
+
     HttpClient client = new DefaultHttpClient();
-    String baseUrl = "http://www.bencarle.com/chess/poll/" + id + "/" + player + "/" + password + "/";
-    HttpGet pollRequest = new HttpGet(baseUrl);
+    HttpGet pollRequest = new HttpGet(baseUrl + "poll/" + credentials + "/");
     HttpGet pushRequest;
-    JSONObject json;
+    JSONObject json = null;
     HttpResponse response;
     BufferedReader reader;
-    
+
     Agent agent = new Agent(board, color, 5);
     byte[] currentMove;
+    int removedPiece;
+
+    System.out.println(board);
     while (true) {
       do {
         response = client.execute(pollRequest);
         reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
         json = new JSONObject(reader.readLine());
+        reader.close();
+        EntityUtils.consume(response.getEntity());
         Thread.sleep(5000);
+        System.out.println("Waiting for the opponents move...");
       } while (!json.getBoolean("ready"));
-      
-      currentMove = getInternalMove(json.getString("lastmove"));
-      board.move(currentMove[0], currentMove[1]);
-      
+
+
+      if (!json.getString("lastmove").isEmpty()) {
+        currentMove = getInternalMove(json.getString("lastmove"));
+        System.out.println("The opponent's move is " + json.getString("lastmove") + ".");
+        removedPiece = board.move(currentMove[0], currentMove[1]);
+        if (Math.abs(removedPiece) == ChessBoard.king) {
+          break;
+        }
+      } else {
+        System.out.println("Oh wait, we're actually going first!");
+      }
+
+      agent.selectBestMove();
       currentMove = agent.getBestMove();
-      board.move(currentMove[0], currentMove[1]);
-      
-      pushRequest = new HttpGet(baseUrl + getCanonicalMove(currentMove) + "/");
-      client.execute(pollRequest);
+      System.out.println("Our move is " + getCanonicalMove(currentMove));
+      pushRequest = new HttpGet(baseUrl + "move/" + credentials + getCanonicalMove(currentMove) + "/");
+      response = client.execute(pushRequest);
+      EntityUtils.consume(response.getEntity());
+      removedPiece = board.move(currentMove[0], currentMove[1]);
+      System.out.println(board);
+      if (Math.abs(removedPiece) == ChessBoard.king) {
+        break;
+      }
     }
+    System.out.println("Game over!");
   }
 
   private static byte[] getInternalMove(String move) {
@@ -61,16 +89,16 @@ public class ShallowBlue {
     byte offset = (byte) ((int) components[0] - 97);
     return (byte) (start + offset);
   }
-  
+
   private static String getCanonicalMove(byte[] move) {
     byte from = move[0];
     byte to = move[1];
     return board.getPieceSymbol(board.getPiece(from)) + toString(from) + toString(to);
   }
-  
+
   private static String toString(byte location) {
     char file = (char) ((location % 10) + 96);
-    int rank =  10 - (location / 10);
+    int rank = 10 - (location / 10);
     return file + "" + rank;
   }
 }
